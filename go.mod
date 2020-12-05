39c61b1f4c0328a4974d0a4fc1fcbc7e6c15bdce
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2020-12-05 23:48:09 +0530

    Completed Instructions

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/ayushbhargav/go-c8
 
-go 1.15
+go 1.2
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 const (
 	memory         = 4096
 	programLoc     = 0x200
@@ -12,10 +17,12 @@ const (
 	byt            = 8
 	bytNib         = 12
 	carry          = 0xF
+	fontNum        = 0xF
 )
 
 type chip8 struct {
 	memory     [memory]byte
+	fontMemory [fontNum]uint16
 	reg        [regCount]uint8
 	instPtr    uint16
 	delayTimer uint8
@@ -23,7 +30,8 @@ type chip8 struct {
 	pc         uint16
 	sp         uint8
 	stack      [stackSize]uint16
-	display    [displayCol * displayRow]bool
+	display    [displayRow][displayCol]uint8
+	keyboard   [regCount]bool
 }
 
 type operation interface {
@@ -41,6 +49,10 @@ func getNibble(opcode uint16, place uint8) uint8 {
 	return uint8(opcode)
 }
 
+func getLast2Nibbles(opcode uint16) uint8 {
+	return uint8(opcode & 0x00FF)
+}
+
 func getLast3Nibbles(opcode uint16) uint16 {
 	return opcode & 0x0FFF
 }
@@ -51,8 +63,10 @@ func getLastByte(opcode uint16) uint8 {
 
 func cls(chip *chip8, opcode uint16) {
 	if opcode == 0x00E0 {
-		for i := 0; i < displayCol*displayRow; i++ {
-			chip.display[i] = false
+		for i := 0; i < displayRow; i++ {
+			for j := 0; i < displayCol; j++ {
+				chip.display[i][j] = 0
+			}
 		}
 	}
 }
@@ -163,11 +177,178 @@ func shrVx(chip *chip8, opcode uint16) {
 
 func subNotVxVy(chip *chip8, opcode uint16) {
 	if getNibble(opcode, 1) == 0x8 && getNibble(opcode, 4) == 0x7 {
-		// TODO: Continue from here.
-		// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+		vx := getNibble(opcode, 2)
+		vy := getNibble(opcode, 3)
+		if chip.reg[vx] > chip.reg[vy] {
+			chip.reg[carry] = 1
+		} else {
+			chip.reg[carry] = 0
+		}
+		chip.reg[vx] = chip.reg[vy] - chip.reg[vx]
+	}
+}
+
+func shiftLeftVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0x8 && getNibble(opcode, 4) == 0xE {
+		vx := getNibble(opcode, 2)
+		if chip.reg[vx]&1<<7 != 0 {
+			chip.reg[carry] = 1
+		} else {
+			chip.reg[carry] = 0
+		}
+		chip.reg[vx] = 2 * chip.reg[vx]
+	}
+}
+
+func skipIfSameVxVy(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0x9 && getNibble(opcode, 4) == 0x0 {
+		vx := getNibble(opcode, 2)
+		vy := getNibble(opcode, 3)
+		if chip.reg[vx] == chip.reg[vy] {
+			chip.pc++
+		}
+	}
+}
+
+func setI(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xA {
+		chip.instPtr = getLast3Nibbles(opcode)
+	}
+}
+
+func jumpI(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xB {
+		chip.instPtr = uint16(chip.reg[0]) + getLast3Nibbles(opcode)
+	}
+}
+
+func randomVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xC {
+		vx := getNibble(opcode, 2)
+		k := getLast2Nibbles(opcode)
+		rand.Seed(time.Now().UnixNano())
+		chip.reg[vx] = uint8(rand.Intn(256)) & k
+	}
+}
+
+func drawVxVy(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xD {
+		vx := chip.reg[getNibble(opcode, 2)]
+		vy := chip.reg[getNibble(opcode, 3)]
+		n := getNibble(opcode, 4)
+		pixelErased := false
+		for i := uint8(0); i < n; i++ {
+			b := chip.memory[chip.instPtr+uint16(i)]
+			for j := uint8(7); j >= 0; j-- {
+				x := (vx + j) % displayCol
+				y := (vy + i) % displayRow
+				bit := b & (0x1 << j)
+				oldPixel := chip.display[y][x]
+				chip.display[y][x] = chip.display[y][x] ^ bit
+				pixelErased = pixelErased || (oldPixel == 1 && chip.display[y][x] == 0)
+			}
+		}
+		if pixelErased {
+			chip.reg[carry] = 1
+		} else {
+			chip.reg[carry] = 0
+		}
+	}
+}
+
+func skipIfPressedVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xE && getNibble(opcode, 3) == 0x9 &&
+		getNibble(opcode, 4) == 0xE {
+		if chip.keyboard[chip.reg[getNibble(opcode, 2)]] {
+			chip.pc++
+		}
+	}
+}
+
+func skipIfNotPressedVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xE && getNibble(opcode, 3) == 0xA &&
+		getNibble(opcode, 4) == 0x1 {
+		if !chip.keyboard[chip.reg[getNibble(opcode, 2)]] {
+			chip.pc++
+		}
+	}
+}
+
+func setDelayTimerToVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x0 &&
+		getNibble(opcode, 4) == 0x7 {
+		chip.reg[getNibble(opcode, 2)] = chip.delayTimer
+	}
+}
+
+func setInputToVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x0 &&
+		getNibble(opcode, 4) == 0xA {
+		vx := getNibble(opcode, 2)
+		for isKeyPressed := false; !isKeyPressed; {
+			for key, keyPress := range chip.keyboard {
+				if keyPress {
+					chip.reg[vx] = uint8(key)
+					isKeyPressed = true
+					break
+				}
+			}
+		}
+	}
+}
+
+func setDelayTimerVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x1 &&
+		getNibble(opcode, 4) == 0x5 {
+		chip.delayTimer = chip.reg[getNibble(opcode, 2)]
+	}
+}
+
+func setSoundTimerVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x1 &&
+		getNibble(opcode, 4) == 0x8 {
+		chip.soundTimer = chip.reg[getNibble(opcode, 2)]
+	}
+}
+
+func setInstrPointerFontLocationVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x2 &&
+		getNibble(opcode, 4) == 0x9 {
+		vx := chip.reg[getNibble(opcode, 2)]
+		chip.instPtr = chip.fontMemory[vx]
+	}
+}
+
+func setInstrPointerBCDVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x3 &&
+		getNibble(opcode, 4) == 0x3 {
+		vx := chip.reg[getNibble(opcode, 2)]
+		for i := uint16(2); i >= 0; i-- {
+			chip.memory[chip.instPtr+i] = vx % 10
+			vx = vx / 10
+		}
+	}
+}
+
+func setRegistersInMemoryVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x5 &&
+		getNibble(opcode, 4) == 0x5 {
+		vx := chip.reg[getNibble(opcode, 2)]
+		for i := uint8(0); i <= vx; i++ {
+			chip.memory[chip.instPtr+uint16(i)] = chip.reg[i]
+		}
+	}
+}
+
+func loadRegistersFromMemoryVx(chip *chip8, opcode uint16) {
+	if getNibble(opcode, 1) == 0xF && getNibble(opcode, 3) == 0x6 &&
+		getNibble(opcode, 4) == 0x5 {
+		vx := chip.reg[getNibble(opcode, 2)]
+		for i := uint8(0); i <= vx; i++ {
+			chip.reg[i] = chip.memory[chip.instPtr+uint16(i)]
+		}
 	}
 }
 
 func main() {
-
 }
